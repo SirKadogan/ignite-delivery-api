875871ddc03bcd6b3a9b2a54572a539e4984a4c6
Thales Kenne <thales@whitespectre.com>
2022-07-09 09:47:07 -0300

    creating deliveryman controller

---
+++ src/modules/deliveryman/usecases/CreateDeliverymanUseCase.ts
@@ -0,0 +1,37 @@
+import { hash } from "bcrypt";
+import prisma from "../../../database/prismaClient";
+
+interface CreateDeliverymanProps {
+  username: string;
+  password: string;
+}
+
+export class CreateDeliverymanUseCase {
+
+  
+  async execute({username, password}: CreateDeliverymanProps) {
+    const deliverymanExists = await prisma.deliveryman.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive"
+        }
+      }
+    })
+
+    if (deliverymanExists) {
+      throw new Error('Deliveryman already exists');
+    }
+
+    const hashPassword = await hash(password, 10);
+
+    const deliveryman = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: hashPassword
+      }
+    })
+
+    return deliveryman;
+  }
+}
