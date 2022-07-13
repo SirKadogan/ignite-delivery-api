c2303bb490d4f73fcc83892fc7fac6216dc1bbed
Thales Kenne <thales@whitespectre.com>
2022-07-12 22:11:56 -0300

    listing deliveries

---
+++ src/middlewares/ensureAuthenticateDeliveryman.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import {verify} from 'jsonwebtoken'
+
+interface Payload {
+  sub: string;
+}
+
+export async function ensureAuthenticateDeliveryman(request: Request, response: Response, next: NextFunction) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return response.status(401).json({
+      message: "Token missing"
+    })
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub } = verify(token, '019acc25a4e242bb77ad489832ada12d') as Payload;
+
+    request.id_deliveryman = sub;
+
+    return next();
+  }catch(err) {
+    return response.status(401).json({
+      message: "Invalid token"
+    })
+  }
+
+
+}
